17473bc1a045008165e619f3b98a1785a4fc8435
Sam Chu <samchupiano@gmail.com>
2025-07-18 18:16:31 +0200

    replace relative urls

--- src/routes/MainMenu.tsx
+++ src/routes/MainMenu.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router"
+import play from "../assets/images/icon-play.svg"
+import logo from "../assets/images/logo.svg"
 
 export default function MainMenu() {
   return (
@@ -8,13 +10,13 @@ export default function MainMenu() {
         className="flex flex-col justify-center items-center rounded-[48px] min-h-[480px] sm:max-w-[592px] sm:w-full sm:mx-auto"
       >
         <div className="max-w-64 mt-[-120px]">
-          <img src="../src/assets/images/logo.svg" alt="logo" />
+          <img src={logo} alt="logo" />
         </div>
         <div
           style={{ boxShadow: "inset 0 -1.5rem 0 .6rem #9d2df5, 0 .4rem 0 .6rem #243041" }}
           className="cursor-pointer mt-16 w-40 h-40 flex justify-center rounded-full bg-[linear-gradient(180deg,_#FE71FE_0%,_#7199FF_100%)]"
         >
-          <img src="../src/assets/images/icon-play.svg" alt="play" />
+          <img src={play} alt="play" />
         </div>
         <Link to="/how-to-play">
           <button className="mt-16 cursor-pointer uppercase w-64 h-16 bg-[#2463FF] text-white text-[32px] rounded-[40px] shadow-[0px_-2px_0px_3px_#3D74FC,_0px_1px_0px_8px_#000] tracking-wider">
