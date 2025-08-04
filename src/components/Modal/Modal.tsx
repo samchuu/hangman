73f74d2540feebe095f268e7b83ac88c8d9bd751
Sam Chu <samchupiano@gmail.com>
2025-08-04 16:58:36 +0200

    add bg hover to MainMenu links

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -24,23 +24,24 @@ export default function Modal({ showModal, setShowModal, result, handlePlayAgain
         <div className="flex flex-col gap-8 items-center justify-center max-w-[275px] mx-auto mt-24 xl:mt-30">
           <button
             onClick={handleAction}
-            className="bg-[#2463FF] px-16 py-3 max-h-16  flex items-center justify-center rounded-[20px] text-white text-center tracking-wider text-2xl md:text-3xl uppercase cursor-pointer hover:bg-[#5C8AFD] transition-colors"
+            className="bg-[#2463FF] px-16 py-3 max-h-16 flex items-center justify-center rounded-[40px] text-white text-center tracking-wider text-2xl md:text-3xl uppercase cursor-pointer hover:bg-[#5C8AFD] transition-colors duration-200"
             style={{ boxShadow: "inset 0 -2px 0 3px rgb(20, 14, 102), inset 0 1px 0 6px rgb(60, 116, 255)" }}
           >
             {result === "Paused" ? "Continue" : "Play again"}
           </button>
           <Link
             to="/category"
-            className="bg-[#2463FF] px-16 py-3 max-h-16 flex items-center justify-center rounded-[20px] text-white text-center tracking-wider text-2xl md:text-3xl uppercase cursor-pointer hover:bg-[#5C8AFD] transition-colors"
+            className="bg-[#2463FF] px-16 py-3 max-h-16 flex items-center justify-center rounded-[40px] text-white text-center tracking-wider text-2xl md:text-3xl uppercase cursor-pointer hover:bg-[#5C8AFD] transition-colors duration-200"
             style={{ boxShadow: "inset 0 -2px 0 3px rgb(20, 14, 102), inset 0 1px 0 6px rgb(60, 116, 255)" }}
           >
             New Category
           </Link>
           <Link
             to="/"
-            className="bg-[linear-gradient(180deg,_#FE71FE_0%,_#7199FF_100%)] relative px-16 py-3 max-h-16 flex items-center justify-center rounded-[20px] text-white text-center tracking-wider text-2xl md:text-3xl uppercase cursor-pointer transition-colors"
+            className="group bg-[linear-gradient(180deg,_#FE71FE_0%,_#7199FF_100%)] relative px-16 py-3 max-h-16 flex items-center justify-center rounded-[40px] text-white text-center tracking-wider text-2xl md:text-3xl uppercase cursor-pointer transition-colors"
             style={{ boxShadow: "inset 0 -2px 0 3px rgb(20, 14, 102), inset 0 1px 0 6px rgb(198, 66, 252)" }}
           >
+            <div className="absolute w-[95%] h-[75%] mx-auto inset-0 top-[8px] rounded-full opacity-0 bg-white group-hover:opacity-20 transition-opacity duration-200 pointer-events-none" />
             Quit Game
           </Link>
         </div>
--- src/routes/MainMenu/MainMenu.tsx
+++ src/routes/MainMenu/MainMenu.tsx
@@ -13,14 +13,18 @@ export default function MainMenu() {
           <img src={logo} alt="logo" />
         </div>
         <Link
-          style={{ boxShadow: "inset 0 -1.5rem 0 .6rem #9d2df5, 0 .4rem 0 .6rem #243041" }}
-          className="cursor-pointer mt-16 w-40 h-40 flex justify-center items-center rounded-full bg-[linear-gradient(180deg,_#FE71FE_0%,_#7199FF_100%)]"
+          style={{ boxShadow: "inset 0 -15px 0 1px #9D2DF5, 0 4px 0 5px #243041" }}
+          className="relative group cursor-pointer mt-16 w-40 h-40 flex justify-center items-center rounded-full bg-[linear-gradient(180deg,_#FE71FE_0%,_#7199FF_100%)]"
           to={"/category"}
         >
-          <img src={play} alt="play" />
+          <div className="absolute inset-0 rounded-full opacity-0 bg-white group-hover:opacity-20 transition-opacity duration-200 pointer-events-none" />
+          <img src={play} alt="play" className="z-10" />
         </Link>
         <Link to="/how-to-play">
-          <button className="mt-16 cursor-pointer uppercase w-64 h-16 bg-[#2463FF] text-white text-[32px] rounded-[40px] shadow-[0px_-2px_0px_3px_#3D74FC,_0px_1px_0px_8px_#000] tracking-wider">
+          <button
+            className="mt-16 cursor-pointer uppercase w-64 h-16 bg-[#2463FF] hover:bg-[#5C8AFD] transition-colors duration-200
+          text-white text-[32px] rounded-[40px] shadow-[0px_-2px_0px_3px_#3D74FC,_0px_1px_0px_8px_#000] tracking-wider"
+          >
             How to play
           </button>
         </Link>
